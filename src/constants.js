35001668efd32f18b4023fa4127fa5ee92ec7c7f
harshjiop <hk8051871496@gmail.com>
2023-12-31 12:13:12 +0530

    video 10 complite

--- src/constants.js
+++ src/constants.js
@@ -8,3 +8,7 @@ export const ACCESS_TOKEN_SECRET =
 export const ACCESS_TOKEN_EXPIRY = "1d";
 export const REFRESH_TOKEN_SECRET = "123";
 export const REFRESH_TOKEN_EXPIRY = "10d";
+
+export const CLOUDINARY_CLOUD_NAME = "ddib2csvf";
+export const CLOUDINARY_API_KEY = 994755516574545;
+export const CLOUDINARY_API_SECRET = "tdxceVOoaBiOAPoJpwb9cigtdDk";
