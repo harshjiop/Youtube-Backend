fa1910033456f194c9b7c42c03e814af80210d78
harshjiop <hk8051871496@gmail.com>
2023-12-31 18:50:18 +0530

    video 13 complite with register Router

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -4,7 +4,7 @@ import {
   CLOUDINARY_API_KEY,
   CLOUDINARY_API_SECRET,
   CLOUDINARY_CLOUD_NAME,
-} from "../constants";
+} from "../constants.js";
 
 cloudinary.config({
   cloud_name: CLOUDINARY_CLOUD_NAME,
